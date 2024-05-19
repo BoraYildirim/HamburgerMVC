8c2e7e7cf46ab2c13726ae98584c80f2700848e2
EmreBekfilavi <embek_95@hotmail.com>
2024-05-19 17:46:08 +0300

    Template Giydirme Layout ve AdminLayout Oluşturma Görsel Düzenlemeler Frontend

--- HamburgerMVC/Controllers/HomeController.cs
+++ HamburgerMVC/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
+using HamburgerMVC.DAL;
 using HamburgerMVC.Models;
+using HamburgerMVC.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -7,20 +9,39 @@ namespace HamburgerMVC.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly BurgerDBContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BurgerDBContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
         {
+            UrunListesi_VM vm = new UrunListesi_VM();
+
+            vm.Hamburgerler = _context.Hamburgers.ToList();
+            vm.Menuler = _context.Menus.ToList();
+            vm.EkMalzemeler = _context.EkMalzemes.ToList();
+
+            return View(vm);
+        }
+
+        public IActionResult Hakkimizda()
+        {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Urunlerimiz()
         {
-            return View();
+			UrunListesi_VM vm = new UrunListesi_VM();
+
+			vm.Hamburgerler = _context.Hamburgers.ToList();
+			vm.Menuler = _context.Menus.ToList();
+			vm.EkMalzemeler = _context.EkMalzemes.ToList();
+
+			return View(vm);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ HamburgerMVC/Models/ViewModels/UrunListesi_VM.cs
@@ -0,0 +1,9 @@
+﻿namespace HamburgerMVC.Models.ViewModels
+{
+    public class UrunListesi_VM
+    {
+        public List<Hamburger> Hamburgerler { get; set; }
+        public List<Menu> Menuler { get; set; }
+        public List<EkMalzeme> EkMalzemeler { get; set; }
+    }
+}
